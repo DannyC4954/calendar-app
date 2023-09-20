2ff01c7d7e3a9406ee2cc01824e6940bacf6dd20
Danny Clayton <49194062+DannyC4954@users.noreply.github.com>
2023-09-20 15:45:28 +0100

    Add files via upload

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function buildCalendar($data)
+function buildCalendar(object $data)
 {
 
     $month = $data->month;
@@ -38,6 +38,58 @@ function buildCalendar($data)
     {
         $days['first'] = $days['first'] - 1;
         $days['starter'] = range(1, $days['first']);
+    }
+
+    return $days;
+
+}
+
+function buildCalendarByMonths(int $numberOfMonths)
+{
+
+    if( empty($numberOfMonths) )
+    {
+        $numberOfMonths = 1;
+    }
+
+    $months = [date('m')];
+    $numberOfMonths = $numberOfMonths;
+    $numberOfMonths = range(1, $numberOfMonths);
+
+    foreach($numberOfMonths as $n)
+    {
+        $months[] = date('m', strtotime('+'.$n.' month', strtotime(date('Y-m-d'))));
+    }
+
+    $days = [];
+
+    foreach($months as $m)
+    {
+
+        $numDays = cal_days_in_month(CAL_GREGORIAN, $m, date('Y'));
+
+        for ($i = 1; $i <= $numDays; $i++) 
+        {
+            $currentDay = sprintf("%04d-%02d-%02d", date('Y'), $m, $i);
+            $currentDay = date("Y-m-d", strtotime($currentDay));
+            $days[$m]['main'][] = $currentDay;
+        }
+
+        $days[$m]['first'] = date('w', strtotime($days[$m]['main'][0]));
+        $days[$m]['starter'] = 0;
+
+        // Resolve issue with Sunday being the first day of the month
+        if( date('l', strtotime($days[$m]['main'][0])) == 'Sunday' )
+        {
+            $days[$m]['first'] = 7;
+        }
+
+        if( $days[$m]['first'] > 0 )
+        {
+            $days[$m]['first'] = $days[$m]['first'] - 1;
+            $days[$m]['starter'] = range(1, $days[$m]['first']);
+        }
+        
     }
 
     return $days;
